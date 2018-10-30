4572b692f5bb17b2799f11626abf74dd4620d38a
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 14:28:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function logDriversByHometown(drivers, hometown) {
 
 function driversByRevenue(drivers) {
   drivers.sort(function (a, b) {
-    return a.revenue.localeCompare(b.revenue);
+    return a.revenue - b.revenue;
   });
 }
