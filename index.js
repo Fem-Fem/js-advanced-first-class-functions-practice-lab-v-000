7934dc4ed8b43ccc5ab53f4373c408e3cd4a005a
Obafemi Fadairo <ofpupp@princeton.edu>
2018-10-30 14:17:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+// logDriverNames() — Receives an array of driver objects and logs the name attribute of each driver to the console.
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
+  });
+};
+
+// const logDriversByHometown = function (drivers, location) {
+//   drivers.forEach(function (driver) {
+//     if (driver.location == location) {
+//       console.log(driver.name)
+//     };
+//   });
+// }
+
+const driversByName = function (drivers) {
+  drivers.sort(drivers);
+};
